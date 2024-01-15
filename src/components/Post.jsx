f7e8209af93d34b8ca89e83211298a729231e2b5
Omar Díaz <omardiazglez@gmail.com>
2024-01-15 19:53:20 +0100

    UPD | env vars and Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,12 +2,18 @@ import PropTypes from "prop-types";
 
 const Post = ({ post }) => {
   console.log(post);
+
   return (
     <div>
       <h2>{post.title}</h2>
       <h3>{post.topic}</h3>
       <p>{post.body}</p>
-      <img src={post.files} />
+      {post.files && (
+        <img
+          src={`${import.meta.env.VITE_BACKEND_URL}/${post.files}`}
+          alt={post.topic}
+        />
+      )}
     </div>
   );
 };
