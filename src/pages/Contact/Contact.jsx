4e3c64c445a9eacecc980bb34c88850b5dd44999
canciox <canciox@gmail.com>
2024-01-26 20:53:46 +0100

    UPD|| Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
 import "./Contact.css";
+import axios from "axios";
 
 const Contact = () => {
-  const [asunto, setAsunto] = useState("");
+  const [subject, setSubject] = useState("");
   const [email, setEmail] = useState("");
-  const [expo, setExpo] = useState("");
+  const [body, setBody] = useState("");
+  const [respuesta, setRespuesta] = useState("");
 
   const handleForm = async (e) => {
     try {
       e.preventDefault();
       const formData = new FormData();
-      formData.append("asunto", asunto);
+      formData.append("subject", subject);
       formData.append("email", email);
-      formData.append("expo", expo);
+      formData.append("body", body);
+
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/contact`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      if (!response) {
+        throw new Error("sin respuesta");
+      }
+      setRespuesta(response.data);
     } catch (error) {
       console.error("No se ha enviado correctamente");
     }
@@ -24,12 +40,12 @@ const Contact = () => {
         <h1>Contacta con nosotros</h1>
         <img
           className="fotoPortada"
-          src="public\fakNews-logo.png"
+          src="../../assets/faknews-logo.svg"
           alt="FotoPortada"
         />
       </section>
       <section>
-        <h2>Un lugar donde todas las noticias tenen su espacio y valor.</h2>
+        <h2>Un lugar donde todas las noticias tienen su espacio y valor.</h2>
         <p>
           FakNews es un periódico digital participativo de buenas noticias
           fundado en el año 2023 y en el que publicamos cada día noticias
@@ -40,16 +56,21 @@ const Contact = () => {
           Colabora con nosotros compartiendo tus noticias con nosotros
         </p>
       </section>
+      {respuesta && (
+        <h3 style={{ backgroundColor: "red", display: "inline-block" }}>
+          {respuesta}
+        </h3>
+      )}
       <form className="contactMessage" onSubmit={handleForm}>
         <fieldset>
           <legend>Contacte con nosostros aquí</legend>
           <div className="labelImputConectForm">
-            <label htmlFor="asunto">Asunto</label>
+            <label htmlFor="subject">Asunto</label>
             <input
               type="text"
-              id="asunto"
-              name="asunto"
-              onChange={(e) => setAsunto(e.target.value)}
+              id="subject"
+              name="subject"
+              onChange={(e) => setSubject(e.target.value)}
               placeholder="Escriba aqui el asunto a tratar..."
               required
             />
@@ -69,9 +90,9 @@ const Contact = () => {
             <label htmlFor="expo">Exponga aqui su consulta</label>
             <input
               type="text"
-              id="expo"
-              name="expo"
-              onChange={(e) => setExpo(e.target.value)}
+              id="body"
+              name="body"
+              onChange={(e) => setBody(e.target.value)}
               placeholder="Exponga su consulta... "
               className="gruesoConsulta"
               required
